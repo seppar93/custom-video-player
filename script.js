be52d8dd39381cf26108b75dedb0e1ade82315d8
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-05-06 17:00:15 -0400

    functionality done

--- script.js
+++ script.js
@@ -17,22 +17,35 @@ stop.addEventListener('click', stopVideo)
 progress.addEventListener('change', setVideoProgress)
 
 // functions
-
 function toggleVideoStatus() {
-  return true
+  video.paused ? video.play() : video.pause()
 }
 
 function updatePlayIcon(){
-  return true
+  video.paused
+    ? (play.innerHTML = `<i class="fa fa-play fa-2x"></i>`)
+    : (play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`);
 }
 function updateProgress() {
-  return true
+  progress.value = (video.currentTime/ video.duration) * 100;
+
+  //  get minutes
+  let mins = Math.floor(video.currentTime / 60)
+  if(mins < 10) mins = '0' + String(mins)
+
+  // get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if(secs < 10) secs = '0' + String(secs)
+
+  timestamp.innerHTML = `${mins} : ${secs}`;
+
 }
 
 function setVideoProgress(){
-  return true
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 function stopVideo(){
-  return true
+  video.currentTime = 0;
+  video.pause()
 }
